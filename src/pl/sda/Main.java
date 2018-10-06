069705d7d5596781728875c1136c13f3c3a45dd0
Remigiusz Rozanski <meystr@gmail.com>
2018-10-06 12:56:17 +0200

    add getMsg method

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
     }
+    private static String getMsg() {
+        return "test";
+    }
+
 }
